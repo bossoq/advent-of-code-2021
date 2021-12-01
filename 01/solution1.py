07248d19bf0d04d1cfce5ef24eecebd75d91e315
Kittipos Wajakajornrit <boss.zz@me.com>
2021-12-01 22:53:17 +0700

    Refactor Code to accept input argument

--- 01/solution1.py
+++ 01/solution1.py
@@ -1,6 +1,18 @@
+import sys
+
 import numpy as np
 
-input_arr = np.loadtxt('input.txt')
+try:
+    input_file = sys.argv[1]
+except IndexError:
+    print("Usage: python solution1.py <input_file>")
+    exit(1)
+
+try:
+    input_arr = np.loadtxt(input_file)
+except IOError:
+    print("Error: Could not open file '{}'".format(input_file))
+    exit(1)
 
 input_arr_l0 = input_arr[:-1]
 input_arr_l1 = input_arr[1:]
