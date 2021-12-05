dbb0fa0fbf83d4627cf6c321b42eb52f5c7a4da7
Kittipos Wajakajornrit <boss.zz@me.com>
2021-12-06 01:25:34 +0700

    Advent of Code 2021 Day 5 with Python

---
+++ 05/solution1.py
@@ -0,0 +1,40 @@
+import sys
+
+import numpy as np
+
+try:
+    input_file = sys.argv[1]
+except IndexError:
+    print("Usage: python solution1.py <input_file>")
+    exit(1)
+
+coords = []
+with open(input_file, "r") as f:
+    for line in f:
+        coordinates = line.strip().split(" -> ")
+        start = coordinates[0].split(",")
+        end = coordinates[1].split(",")
+        coord = [start, end]
+        coords.append(coord)
+
+coords = np.array(coords).astype(int)
+
+max_coord = coords.max()
+
+mapped = np.zeros((max_coord + 1, max_coord + 1), dtype=int)
+
+for coord in coords:
+    # [2, 2] -> [2, 1]
+    if coord[0][0] == coord[1][0]:
+        first = min(coord[0][1], coord[1][1])
+        second = max(coord[0][1], coord[1][1]) + 1
+        mapped[first:second, coord[0][0]] += 1
+    # [0, 9] -> [5, 9]
+    elif coord[0][1] == coord[1][1]:
+        first = min(coord[0][0], coord[1][0])
+        second = max(coord[0][0], coord[1][0]) + 1
+        mapped[coord[0][1], first:second] += 1
+
+total_overlap = (mapped > 1).sum()
+print(mapped)
+print(f"Total Overlap Point: {total_overlap}")
