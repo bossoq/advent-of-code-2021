0ef07aa5a759672c604222a9377ed7ff87842a08
Kittipos Wajakajornrit <boss.zz@me.com>
2021-12-06 16:51:07 +0700

    Advent of Code 2021 Day 6 with Python

---
+++ 06/solution1.py
@@ -0,0 +1,28 @@
+import sys
+
+try:
+    input_file = sys.argv[1]
+except IndexError:
+    print("Usage: python solution1.py <input_file>")
+    exit(1)
+
+old_timer = 6
+new_timer = 8
+
+total_day = 80
+
+with open(input_file, "r") as f:
+    input_list = f.readline().split(",")
+    input_list = [int(i) for i in input_list]
+
+all_counts = [0] * (new_timer + 1)
+
+for initial in input_list:
+    all_counts[initial] += 1
+for day in range(total_day):
+    current = all_counts.pop(0)
+    all_counts[old_timer] += current
+    all_counts.append(current)
+
+lanternfish_count = sum(all_counts)
+print(f"Total lanternfish count: {lanternfish_count}")
