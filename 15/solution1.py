2416e7e612d25f3617159d6e89fc40465faa6be9
Kittipos Wajakajornrit <boss.zz@me.com>
2021-12-15 22:20:00 +0700

    Advent of Code 2021 Day 15 part a with Python

---
+++ 15/solution1.py
@@ -0,0 +1,53 @@
+from collections import deque
+import sys
+
+try:
+    input_file = sys.argv[1]
+except IndexError:
+    print("Usage: python solution1.py <input_file>")
+    exit(1)
+
+input_dict = dict()
+
+with open(input_file, "r") as f:
+    for ridx, row in enumerate(f):
+        for cidx, col in enumerate(row.strip()):
+            input_dict[cidx + 1j * ridx] = int(col)
+    col_count = len(row)
+    row_count = int(len(input_dict) / col_count)
+
+
+def get_neighbors(pos):
+    neighbors = []
+    if pos.real > 0:
+        neighbors.append(pos.real - 1 + pos.imag * 1j)
+    if pos.real < col_count - 1:
+        neighbors.append(pos.real + 1 + pos.imag * 1j)
+    if pos.imag > 0:
+        neighbors.append(pos.real + (pos.imag - 1) * 1j)
+    if pos.imag < row_count - 1:
+        neighbors.append(pos.real + (pos.imag + 1) * 1j)
+    return neighbors
+
+
+def main():
+    dists = dict()
+    tovisit = deque()
+    tovisit.append(0 + 0j)
+    visited = set()
+    dists[0 + 0j] = 0
+
+    while len(tovisit) > 0:
+        node = tovisit.popleft()
+        visited.add(node)
+
+        for neighbor in get_neighbors(node):
+            nextdist = dists[node] + input_dict[neighbor]
+            if neighbor not in visited and dists.get(neighbor, 10000000) > nextdist:
+                dists[neighbor] = nextdist
+                tovisit.append(neighbor)
+    return dists[col_count - 1 + (row_count - 1) * 1j]
+
+
+risk_level = main()
+print(f"Lowest Risk Level: {risk_level}")
